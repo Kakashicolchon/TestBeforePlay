9d7080948c7a9d5e6d744a2a7cf32dc9037ae073
Kakashicolchon <jitsu.gokei@hotmail.com>
2019-09-12 18:35:03 +0200

    cambiada dirección de icono a una dirección relativa

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -5,15 +5,15 @@ public class MainMenu {
     private JPanel panelMain;
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ping Tester");
-        ImageIcon img = new ImageIcon("C:\\Users\\jitsu\\IdeaProjects\\TestBeforePlay\\assets\\icon.jpg");
-        //Línea de encima funciona pero mejor poner con dirección relativa.
+        ImageIcon img = new ImageIcon("./assets/icon.jpg");//Dirección del icono de la ventana con dirección relativa
         frame.setIconImage(img.getImage()); //Poniendo la imagen de icono
         frame.setContentPane(new MainMenu().panelMain); //Haciendo que el panel se una a la ventana como contenido
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Si le damos a la cruz la aplicación se cierra
         frame.pack(); //TODO Buscar que signficica esta línea
         frame.setVisible(true); //Hacer que la ventana sea visible
-        //Test
-        //TODO testeando si funciona
+    }
+    public void buttonClickTest(){
+        System.out.println("Testing button");
     }
 }
 
