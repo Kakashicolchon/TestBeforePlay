3380901e7763e3e73f0ed95a511ef57b8d4b1821
Kakashicolchon <jitsu.gokei@hotmail.com>
2019-09-12 18:49:09 +0200

    Añadid estrucutra inicial de un botón y prueba

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,8 +1,15 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu {
     private JButton button1;
     private JPanel panelMain;
+
+    private MainMenu() {
+        button1.addActionListener(e -> System.out.println("TEsting botón"));
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Ping Tester");
         ImageIcon img = new ImageIcon("./assets/icon.jpg");//Dirección del icono de la ventana con dirección relativa
@@ -11,9 +18,6 @@ public class MainMenu {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Si le damos a la cruz la aplicación se cierra
         frame.pack(); //TODO Buscar que signficica esta línea
         frame.setVisible(true); //Hacer que la ventana sea visible
-    }
-    public void buttonClickTest(){
-        System.out.println("Testing button");
     }
 }
 
