0916c4bd3dad093f2be20db50bf83d05f6e5c9f5
Kakashicolchon <jitsu.gokei@hotmail.com>
2019-09-12 19:47:46 +0200

    Falta añadir el settext del título

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,23 +1,31 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class MainMenu {
     private JButton button1;
     private JPanel panelMain;
+    private JLabel title;
 
     private MainMenu() {
         button1.addActionListener(e -> System.out.println("TEsting botón"));
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Ping Tester");
+        JFrame frame = new JFrame("Ping Tester"); //Primero creamos un objeto tipo Java Frame, una ventana nueva y le damos un título
+        frame.setLocationRelativeTo(null);//centramos la ventana
         ImageIcon img = new ImageIcon("./assets/icon.jpg");//Dirección del icono de la ventana con dirección relativa
         frame.setIconImage(img.getImage()); //Poniendo la imagen de icono
         frame.setContentPane(new MainMenu().panelMain); //Haciendo que el panel se una a la ventana como contenido
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Si le damos a la cruz la aplicación se cierra
         frame.pack(); //TODO Buscar que signficica esta línea
         frame.setVisible(true); //Hacer que la ventana sea visible
+
+        JLabel title = new JLabel();
+        TitleObject titleObject = new TitleObject("asdas", title);
+        //TODO hacer el settext del título
     }
+
+    //--------------------------------METHODS-------------------------------------
+
 }
 
---
+++ src/TitleObject.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TitleObject {
+
+    public TitleObject(String titleText, JLabel jLabelTitle) {
+        jLabelTitle.setText(titleText);
+    }
+
+
+}
